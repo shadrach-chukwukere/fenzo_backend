8bc6fe71f2d0652b834b64a7af16f18d0a243c0f
Shadrach Chukwukere <shadrachchukwukerechinemerem@gmail.com>
2025-08-11 23:59:36 +0100

    eee

--- server1.js
+++ server1.js
@@ -7,6 +7,7 @@ import bcrypt from "bcrypt";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 // Fix __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
